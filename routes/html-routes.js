efdfd0e1353fbdf4abe9122712e452ac526daea8
Alejandro Rivera Jr <30929447+AlejandroRiveraJr@users.noreply.github.com>
2018-05-26 16:51:05 -0400

    Add files via upload
    HTML-Routes and connection.js

---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "..\views\index.html"));
+  });
+};
