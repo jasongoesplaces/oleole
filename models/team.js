7b29255b7a3588dd88bd28ad926e9944335eda11
Ryan <Admin@MacBook-Pro.local>
2018-06-02 19:46:05 -0400

    added defense and offense tables

--- models/team.js
+++ models/team.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
     var Teams = sequelize.define("Teams", {
-      // Giving the Author model a name of type STRING
+      //  Teams model 
       name: DataTypes.STRING,
       wins: DataTypes.INTEGER,
       losses: DataTypes.INTEGER,
@@ -11,4 +11,29 @@ module.exports = function(sequelize, DataTypes) {
         timestamps: false
     });
     return Teams
+
+    var Offense = sequelize.define("Offense", {
+        // Offense model 
+        name: DataTypes.STRING,
+        player: DataTypes.STRING,
+        goals: DataTypes.INTEGER,
+        sog: DataTypes.INTEGER,
+        assists: DataTypes.INTEGER
+      }, {
+          timestamps: false
+      });
+      return Offense
+
+      var Defense = sequelize.define("Defense", {
+        // Defense model 
+        name: DataTypes.STRING,
+        player: DataTypes.STRING,
+        SA: DataTypes.INTEGER,
+        GA: DataTypes.INTEGER,
+        saves: DataTypes.INTEGER
+      }, {
+          timestamps: false
+      });
+      return Defense
+  
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,4 +9,10 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "..\views\index.html"));
   });
+
+  app.get("/teams", function(req, res) {
+    res.sendFile(path.join(__dirname, "..\views\team.html"));
+  });
+
 };
+
