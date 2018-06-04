1ba4b2a8d83acee5241de93a668fd06b66561d35
Jason <jasongoesplaces@gmail.com>
2018-06-04 16:41:07 -0400

    added html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,6 +7,14 @@ module.exports = function(app) {
 
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "..\views\index.html"));
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
+
+  app.get("/dashboard", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/dashboard.html"));
+  });
+
+  app.get("/team", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/team.html"))
+  })
 };
